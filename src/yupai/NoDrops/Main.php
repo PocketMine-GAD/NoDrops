27fdcde13f91d3a2468a97ea49b0ceb1948da614
PocketMine-GAD <13420098228zy2@gmail.com>
2016-04-23 00:03:41 +0800

    Update Main.php

--- src/yupai/NoDrops/Main.php
+++ src/yupai/NoDrops/Main.php
@@ -9,20 +9,36 @@ use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+
 
 class Main extends PluginBase implements Listener {
 
     public function OnEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this ,$this);
-        $this->getLogger()->info(TextFormat::GREEN . "NoDrops by Yupai Enable!");
+        $this->getLogger()->info(TextFormat::GREEN . "■■■■■■■■■■NoDrops by Yupai Enable!■■■■■■■■■");
+        @mkdir($this->getDataFolder());
+		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array(
+		"item"=>"264"
+		));
+    }
+    public function OnPlayer(PlayerJoinEvent $j){
+    $player=$j->getPlayer();
+    $player->sendMessage("§cThis server can't §4drop item§c which in server config setting !");
     }
 
     public function OnDisable(){
-        $this->getLogger()->info(TextFormat::RED . "NoDrops by Yupai Disable!");
+        $this->getLogger()->info(TextFormat::RED . "■■■■■■■■■■ NoDrops by Yupai Disable!■■■■■■■■■■" );
     }
 
     public function onDrop(PlayerDropItemEvent $event){
         $player = $event->getPlayer();
+        if($event->getItem()->getId()!=$this->config->get("item")){
+        return;
+        }
         $event->setCancelled(true);
+        $n=$player->getName();
+        $player->sendTip("§4You can't drop those items! \n          §a$n");
     } 
 }
